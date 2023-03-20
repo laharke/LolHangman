c6f16109a8663e5804d036581260e9895e6b7dc2
laharke <ronconimauro@yahoo.com.ar>
2023-03-20 00:43:38 -0300

    Added image and qutoe from champion when game is over

--- hangman.js
+++ hangman.js
@@ -1,4 +1,8 @@
 import { Champion } from "./champion.js";
+
+const $finishedDiv = document.querySelector('#gameFinished');
+const $finishedIMG = document.querySelector('#gameFinishedIMG');
+const $finishedPAR = document.querySelector('#gameFinishedTEXT');
 let finish;
 const $btnReset = document.querySelector("#btnReset");
 const getChampionsAsync = async () => {
@@ -8,20 +12,9 @@ const getChampionsAsync = async () => {
   } catch (error) {
     console.error(error.message);
   }
-  finally {
-  }
 };
-/*const champions = [
-    "Aatrox",
-    "Ahri",
-    "Akali",
-    "Alistar",
-    "Amumu",
-    "Anivia",
-    "Annie",
-    // Add more champions here...
-  ];*/
-  var randomProperty = function (obj) {
+
+ var randomProperty = function (obj) {
     var keys = Object.keys(obj);
     return obj[keys[ keys.length * Math.random() << 0]];
   };
@@ -32,11 +25,14 @@ const getChampionsAsync = async () => {
   //const randomIndex = Math.floor(Math.random() * champions.length);
   //const randomChampion = champions[randomIndex].toUpperCase();
   let randomChampion;
-  let datos;
+  let datos = await getChampionsAsync();
+  datos = datos.data;
   // Set up the game state
   let remainingGuesses;
   let guessedLetters;
   let wordInProgress;
+
+  
 
   // Define a function to update the display
   function updateDisplay() {
@@ -54,14 +50,46 @@ const getChampionsAsync = async () => {
   
     // Check for a win or loss
     if (!wordInProgress.includes("_")) {
-      finish = true;
-      alert("You win!");
+      win();
+      
+      
+      //hacer funcion win y lose para tener todo.
     } else if (remainingGuesses === 0) {
-      finish = true;
-      alert(`You lose! The champion was ${randomChampion}.`);
+      lose(); 
     }
   }
-  
+  //probalmetne se puedan combinar las funcione slseo y win porqeu son iguales mtemos un IF pero bueno VEMOS XDDD 
+  function win(){
+    finish = true;
+    alert("You win!");
+    //hardcoedaa la version dsp por ahi hay que cambiarlo 
+    let imageUrl = `http://ddragon.leagueoflegends.com/cdn/11.7.1/img/champion/${champion.name}.png`;
+    let championImage = document.createElement('img');
+    let championMensaje = document.createElement('p');
+    let text = document.createTextNode(champion.blurb);
+    championMensaje.appendChild(text);
+    championImage.src = imageUrl;
+
+    $finishedIMG.appendChild(championImage);
+    $finishedPAR.appendChild(championMensaje);
+  }
+
+  function lose(){
+    finish = true;
+    alert(`You lose! The champion was ${randomChampion}.`);
+    //hardcoedaa la version dsp por ahi hay que cambiarlo 
+    let imageUrl = `http://ddragon.leagueoflegends.com/cdn/11.7.1/img/champion/${champion.name}.png`;
+    let championImage = document.createElement('img');
+    let championMensaje = document.createElement('p');
+    let text = document.createTextNode(champion.blurb);
+    championMensaje.appendChild(text);
+    championImage.src = imageUrl;
+
+    $finishedIMG.appendChild(championImage);
+    $finishedPAR.appendChild(championMensaje);
+  }
+
+
   // Define a function to handle guesses
   function handleGuess(letter) {
     if (guessedLetters.includes(letter)) {
@@ -88,9 +116,9 @@ const getChampionsAsync = async () => {
   document.addEventListener("keydown", event => {
     if (event.keyCode >= 65 && event.keyCode <= 90) {
       const letter = event.key.toUpperCase();
-      console.log(letter);
-      if(!finish)
+      if(!finish){
         handleGuess(letter);
+      }
     }
   });
   
@@ -99,22 +127,23 @@ const getChampionsAsync = async () => {
     updateDisplay();
   })
   // Initialize the display
-  await initVariables();
+  initVariables();
   updateDisplay();
 
-async function  initVariables(){
+function  initVariables(){
     finish = false;
-    datos = await getChampionsAsync();
-    datos = datos.data;
-    console.log(datos);
+    // en datos.data estan los champions en forma de objeto 
     //Obtengo todos los key y value del champion.
     let championObj = randomProperty(datos);
+    console.log(datos);
     //Creo el champion seleccionado las propiedades que ya definimos en la clase.
     champion = new Champion(championObj.id, championObj.title,championObj.partype,championObj.tags,championObj.blurb);
     randomChampion = champion.name.toUpperCase();
-  
+    $finishedIMG.innerHTML = ' ';
+    $finishedPAR.innerHTML = ' ';
+    
   // Set up the game state
-    remainingGuesses = 6;
+    remainingGuesses = 5;
     guessedLetters = [];
     wordInProgress = Array.from(randomChampion, () => "_");
   }
